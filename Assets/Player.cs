f5c42fedbd98b7d94defc513fd23f73fb0ff9536
coosdog <hojun2584@gmail.com>
2023-12-12 19:14:15 +0900

    Update Player.cs

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -7,7 +7,9 @@ public class Player : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        transform.rotation = Quaternion.identity;   
         Debug.Log("TEST");
+
         Debug.Log("DDDD");
     }
 
